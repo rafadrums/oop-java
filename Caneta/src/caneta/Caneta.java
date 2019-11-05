9fb5373aae47fdb907663b2178e8a1ccc5cdba48
Rafael Bueno <50866088+rafadrums@users.noreply.github.com>
2019-11-05 12:47:27 -0300

    structured (constructor, setters&getters)

--- Caneta/src/caneta/Caneta.java
+++ Caneta/src/caneta/Caneta.java
@@ -1,29 +1,76 @@
 package caneta;
 
-public class Caneta {
-
-    public String modelo;
-    public String cor;
+public class Caneta { 
+    private String modelo;
     private float ponta;
+    public String cor;
     protected int carga;
     private boolean tampada;
-
-    public void status() {
-        System.out.println("Modelo: " + this.modelo);
-        System.out.println("Cor: " + this.cor);
-        System.out.println("Ponta: " + this.ponta);
-        System.out.println("Carga: " + this.carga);
-        System.out.println("Esta tampada? " + this.tampada);
+    
+    //exemplo construtor 1
+    
+    /*  -> exemplo
+    public Caneta(){ //já executará ações
+        this.tampar();
+        this.cor = "Azul";
+    }
+*/
+    
+    //CONSTRUTOR
+    public Caneta(String m, String c, float p){
+        this.modelo = m;
+        this.cor = c;
+        this.ponta = p;
+        this.tampar();
+    }
+    
+    
+    
+    //Getter e setters
+    public String getModelo(){
+        return this.modelo;
+    }
+    
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    
+    public float getPonta(){
+        return this.ponta;
+    }
+    
+    public void setPonta(float ponta){
+        this.ponta = ponta;
     }
 
-    public void rabiscar() {
-        if (this.tampada == true) {
-            System.out.println("ERRO! Não posso rabiscar!");
-        } else {
-            System.out.println("Estou rabiscando");
-        }
+    public String getCor() {
+        return cor;
     }
 
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+    
+    
+    
+    //Metodos
+    
     public void tampar() {
         this.tampada = true;
     }
@@ -31,5 +78,13 @@ public class Caneta {
     public void destampar() {
         this.tampada = false;
     }
-
+     
+    public void status(){
+        System.out.println("SOBRE A CANETA: ");
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Esta tampada? " + this.tampada);
+    }
 }
--- Caneta/src/caneta/MainCaneta.java
+++ Caneta/src/caneta/MainCaneta.java
@@ -3,20 +3,23 @@ package caneta;
 public class MainCaneta {
     
      public static void main(String[] args) {
-     
-         Caneta c1 = new Caneta();
-         c1.modelo = "bic cristal";
-         c1.cor = "Caneta azul";
-         //c1.ponta = 0,5f;
-         //c1.tampada = false;
-         c1.carga = 80;
-         c1.destampar();
-         c1.status();
-         c1.rabiscar();
-         
-         
-         
-         
-         
-    }
+        Caneta c1 = new Caneta("BIC", "Azul", 0.5f);
+        
+        /*
+        c1.setModelo("BIC");
+        //c1.modelo = "BIC";(private)
+    
+        c1.setPonta(0.5f);
+        //c1.ponta = 0.5f;(private)
+        
+        c1.status();
+        System.out.println("");
+        
+        System.out.println("Tenho uma caneta " + c1.getModelo() 
+                + " de ponta " + c1.getPonta());    
+    
+*/
+        c1.status(); 
+    
+  }  
 }
